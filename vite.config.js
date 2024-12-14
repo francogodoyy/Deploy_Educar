0981f921a096f7815ab90d4cdf4fd72d02466775
francogodoyy <alanfrancogodoy@gmail.com>
2024-12-13 22:15:37 -0300

    changes again

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: './', // Define una base relativa para el proyecto
   build: {
-    outDir: 'dist' // Carpeta de salida para el build
+    outDir: 'public' // Carpeta de salida para el build
   },
   server: {
     proxy: {
